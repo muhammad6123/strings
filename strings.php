645c07787cb9bc46de88d2ee9c9e033fbea26e59
muhammad6123 <mamuhammad682@gmail.com>
2024-01-09 01:19:17 +0600

    strings

--- strings.php
+++ strings.php
@@ -12,9 +12,18 @@
 echo "</br>";
 
 $x = "php strings";
+$y = "php is nice";
+$z = "js is nice";
 echo strlen($x);
 echo "</br>";
 echo str_word_count($x);
+echo "</br>";
+echo strpos($y, "nice");
+echo "</br>";
+ echo str_replace("js", "php", $z)
+
+
+
 
 
 ?>
