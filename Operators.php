9ba710dcb6d4a9d7ff242a1e1fa132b1a065d7b8
muhammad6123 <mamuhammad682@gmail.com>
2024-01-09 09:42:42 +0600

    operators

---
+++ Operators.php
@@ -0,0 +1,35 @@
+<?php 
+$y = 15;
+$x = 5;
+echo $y + $x;
+echo "</br>";
+echo $y - $x;
+echo "</br>";
+echo $y * $x;
+echo "</br>";
+echo $y / $x;
+echo "</br>";
+echo $y % $x;
+
+// Assignment Operators
+
+echo"<br>";
+
+$s = 20;
+$s +=10;
+echo $s;
+echo "</br>";
+$s -=10;
+echo $s;
+echo "</br>";
+
+$s *=10;
+echo $s;
+echo "</br>";
+$s /=35;
+echo $s;
+echo "</br>";
+
+
+
+?>
