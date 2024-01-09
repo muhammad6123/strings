498171f081fc34141b62e20983d3a83a224850eb
muhammad6123 <mamuhammad682@gmail.com>
2024-01-09 10:40:27 +0600

    logical opertors

---
+++ Logical_Operators.php
@@ -0,0 +1,24 @@
+<?php 
+$x = 30;
+$y = 20;
+
+if($x == 30 and $y == 20){
+    echo "php is nice";
+}
+else{
+    echo "not file";
+}
+echo "</br>";
+$x = 30;
+$y = 20;
+
+if($x == 30 and $y == 10){
+    echo "php is nice";
+}
+else{
+    echo "not file";
+}
+echo "</br>";
+
+
+?>
