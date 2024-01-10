71697b780fb7056cf64f13f7ab06f6030b1abac1
muhammad6123 <mamuhammad682@gmail.com>
2024-01-11 00:14:48 +0600

    array01

--- array.php
+++ array.php
@@ -17,6 +17,31 @@ foreach ($ages as $s => $age) {
     echo "key = " . $s . ", value = " . $age;
     echo "<br/>";
 }
+echo "<br/>";
+$cars = array(
+    array("bmw", 14, 58),
+    array("valoe", 48, 65),
+    array("saab", 14, 58)
+);
+echo $cars[1][2];
+echo "<br/>";
 
+$sub = array(
+    array("bmw", 14, 58),
+    array("valoe", 48, 65),
+    array("saab", 14, 58),
+    array("nice",15, 23)
+);
 
+for($row = 0; $row<4; $row++){
+    echo "<p> row namber $row </p>";
+    echo "<ul>";
+    for($col = 0; $col <3; $col++){
+       echo "<li>".$sub[$row][$col]."</li>"; 
+
+    }
+    echo "</ul>";
+}
+
+$nam = array()
 ?>
