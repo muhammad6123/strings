7af0969b12971b6b1b29ac33a7b3beeeebd8ad7a
muhammad6123 <mamuhammad682@gmail.com>
2024-01-12 02:47:14 +0600

    time

--- from.php
+++ from.php
@@ -1,14 +1,15 @@
 <?php 
+$name = $email = $comment = $gender = "";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $name = ($_POST["name"]);
     $email = ($_POST["email"]);
     $comment = ($_POST["comment"]);
     $gender = ($_POST["gender"]);
 
-    echo "name".$name ."<br/>";
-    echo "email".$email."<br/>";
-    echo "comment".$comment."<br/>";
-    echo "gender".$gender;
+    echo "name: ".$name ."<br/>";
+    echo "email: ".$email."<br/>";
+    echo "comment: ".$comment."<br/>";
+    echo "gender: ".$gender;
 
 
 }
@@ -30,17 +31,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <form action="<?php echo ($_SERVER['PHP_SELF']);?>" method="post">
     <table>
         <tr>
-            <td>name</td>
+            <td>name :</td>
             <td><input type="text" name="name"></td>
         </tr>
         
         <tr>
-            <td>email</td>
+            <td>email :</td>
             <td><input type="text" name="email"></td>
         </tr>
 
         <tr>
-            <td>comment</td>
+            <td>comment :</td>
             <td><textarea name="comment" cols="30" rows="10"></textarea></td>
         </tr>
 
