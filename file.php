16cb1e2d955c6efbd533d44cfc6d4b7ab24440a0
muhammad6123 <mamuhammad682@gmail.com>
2024-01-13 00:34:54 +0600

    file

---
+++ file.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php echo ($_SERVER['PHP_SELF']);?>" enctype="multipart/form-data" method="post">
+    <input type="file" name="image"/>
+    <input type="submit" value="submit"/>
+    </form>
+<?php
+    if(isset ($_FILES['image'])){
+        $filename = $_FILES['image'] ['name'];
+        $filetmp = $_FILES['image'] ['tmp_name'];
+        move_uploaded_file($filetmp, "image/".$filename);
+        echo "image uploaded successfully";
+    }
+
+    ?>
+</body>
+</html>
