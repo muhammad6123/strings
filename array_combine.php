e5a3d8355ef6bbe388483924294ed1598fd4ccc2
muhammad6123 <mamuhammad682@gmail.com>
2024-01-14 01:36:29 +0600

    array_combine

---
+++ array_combine.php
@@ -0,0 +1,11 @@
+<?php 
+$name = array("akbor", "mamun", "ratul");
+$age = array ("25", "26", "58");
+$combine  = array_combine($name, $age);
+print("<pre>");
+
+print_r ($combine);
+print ("<pre/>");
+
+
+?>
