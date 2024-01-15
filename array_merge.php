04c828a2ffb5ad6e8395354c008ea45dc343a0f1
muhammad6123 <mamuhammad682@gmail.com>
2024-01-16 00:53:26 +0600

    array merge

---
+++ array_merge.php
@@ -0,0 +1,11 @@
+<?php 
+$arr_one = array("red", "green");
+$arr_two = array("blue", "yellow");
+$result = array_merge($arr_one, $arr_two);
+
+
+
+print("<pre>");
+print_r($result);
+print("</pre>");
+?>
